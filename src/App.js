41ffacf0111b1aef7f47c4f57032fc0947ae77bb
FedeSca001 <familadore@gmail.com>
2022-11-21 15:01:00 +0100

    add props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 import CardComponent from './components/CardRestorants/CardComponent';
 
 function App() {
+  let restorants = [];
+  const getData = async () => {
+          const url =
+                  "https://62630e3ac430dc560d2b8a72.mockapi.io/restorants";
+          const r = await fetch(url);
+          restorants = await r.json();
+          console.log(restorants[0]);
+  };
+  getData();
   return (
     <div className="App">
       <header className="App-header">
--- src/components/CardRestorants/CardComponent.jsx
+++ src/components/CardRestorants/CardComponent.jsx
@@ -1,19 +1,10 @@
 import './CardRestorant.css'
 
-function CardComponent (){
-    let restorants = [];
-    const getData = async () => {
-        const url =
-        "https://62630e3ac430dc560d2b8a72.mockapi.io/restorants";
-        const r = await fetch(url);
-        restorants = await r.json();
-        console.log(restorants[0]);
-    };
-    getData();
+function CardComponent (props){
     return (
         <div className='contenedor-card'>
-            <img src="img" alt="img" />
-            <h3>Restorant Name:</h3>
+            <img src={props.avatar} alt="img" />
+            <h3>Restorant Name:{props.name}</h3>
             <p>All Foods</p>
         </div>
     );
