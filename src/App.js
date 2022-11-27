f23ec569a11e1a4d171c636aa0e7984134716a20
FedeSca001 <familadore@gmail.com>
2022-11-27 20:44:57 +0100

    add useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
 import CardComponent from './components/CardRestorants/CardComponent';
-let restorants = [];
-const getData = async () => {
-  const url =
-          "https://62630e3ac430dc560d2b8a72.mockapi.io/restorants";
-  const r = await fetch(url);
-  return restorants = await r.json();
-};
-getData();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Comienza React</h1>
-        <CardComponent objects={restorants} />
+        <CardComponent />
       </header>
     </div>
   );
--- src/components/CardRestorants/CardComponent.jsx
+++ src/components/CardRestorants/CardComponent.jsx
@@ -1,12 +1,22 @@
-import './CardRestorant.css'
+import './CardRestorant.css';
+import React, { useState } from "react";
 
-function CardComponent (props){
-    console.log(props.objects);
+function CardComponent (){
+    const [item, setItem] = useState([]);
+    const addItem = ()=>{
+        setItem([...item, {
+                id: item.length,
+                name: `Juan carlos ${item.length}`,
+        }]);
+    }
     return (
         <div className='contenedor-card'>
-            <img alt="img" />
-            <h3>Restorant Name:</h3>
-            <p>All Foods</p>
+            <button onClick={addItem}>Add item</button>
+            <ul>{
+                item.map(elem => (
+                    <li key={elem.id}>{elem.name}</li>
+                ))
+            }</ul>
         </div>
     );
 }
